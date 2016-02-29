89ef93aad758dcfb72d1299f34ec28168a4e7c00
Prasanna <prasanna.jugade@click2cloud.net>
2016-02-29 10:39:26 +0530

    commit 1

---
+++ src/WebApplication4/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
+
+namespace WebApplication4
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        // This method gets called by a runtime.
+        // Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            //var builder = new ConfigurationBuilder();
+            //builder.Add(new MemoryConfigurationProvider());
+            //var config = builder.Build();
+            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
+            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
+            // services.AddWebApiConventions();
+            //var configuration = new Microsoft.Extensions.Configuration.ConfigurationBuilder()
+            //    .AddJsonFile("config.json")
+            //    .AddEnvironmentVariables();
+
+            //var connectionString = configuration.Get("Data:DefaultConnection:ConnectionString");
+        }
+
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.MinimumLevel = LogLevel.Information;
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+
+            // Add the platform handler to the request pipeline.
+            app.UseIISPlatformHandler();
+
+            // Configure the HTTP request pipeline.
+            app.UseStaticFiles();
+
+            // Add MVC to the request pipeline.
+            app.UseMvc();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+            // Add the following route for porting Web API 2 controllers.
+            // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+        }
+    }
+}
